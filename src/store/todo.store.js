ce8f4ab7d758724579f27c189ef9c64de5d4c56c
Javier Saez <banbleac@gmail.com>
2022-12-15 15:39:50 +0100

    Add Todo

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -2,7 +2,7 @@ import { Todo } from "../todos/models/todo.model";
 /**
  * Filters esta en mayusculas porque es una enumeracion
  * 
-*/ 
+*/
 const Filters = {
     All: 'all',
     Completed: 'Completed',
@@ -13,7 +13,9 @@ const state = {
     todos: [
         new Todo('Piedra del Alma'),
         new Todo('Piedra del Infinito'),
-        new Todo('Piedra del Tiempo'),     
+        new Todo('Piedra del Tiempo'),
+        new Todo('Piedra del Poder'),
+        new Todo('Piedra de la Luz'),
 
     ],
     filter: Filters.All,
@@ -25,6 +27,78 @@ const initStore = () => {
     console.log('InitStore');
 }
 
+
+
+const loadStore = () => {
+    throw new Error('Not Implemented');
+}
+
+const getTodos = ( filter = Filters.All ) => {
+    switch( filter ) {
+        case Filters.All:
+            return [...state.todos];
+        case Filters.Completed:
+            return state.filter( todo => todo.done );
+        case Filters.Pending:
+            return state.filter( todo => !todo.done);
+        default:
+            throw new Error(`Option ${ filter } is not valid`);
+    }
+}
+
+/**
+ * Addtodo: addTodo  function
+ * @param {String} description
+ */
+const addTodo = ( description ) => {
+    if( !description ) throw new Error('Not Implemented');
+    state.todos.push( new Todo( description ) );
+}
+
+/**
+ * Update Todo:  toggleTodo function
+ * @param {String} todoId 
+ * return  
+ */
+const toggleTodo = (todoId) => {
+    state.todos = state.todos.map( todo => {
+       if (todo.id === todoId ){
+        todo.done = !todo.done;
+       }
+       return todo;
+    })
+}
+
+/**
+ * Delete Todo: {deleteTodo} function
+ * @param {String} todoId 
+ */
+const deleteTodo = (todoId) => {
+    state.todos = state.filter( todo => todo.id !== todoId );
+}
+
+const deleteComleted = () => {
+    state.todos = state.filter( todo => todo.done );
+}
+ /**
+  * 
+  * @param {Filters} newFilter 
+  */
+const setFilter = (newFilter = Filters.All) => {
+    state.filter = newFilter;
+}
+
+const getCurrentFilter = () => {
+    return state.filter;
+}
 export default {
     initStore,
+    loadStore,
+    addTodo,
+    toggleTodo,
+    deleteTodo,
+    deleteComleted,
+    setFilter,
+    getCurrentFilter,
+    getTodos,
 }
--- src/todos/app.js
+++ src/todos/app.js
@@ -1,17 +1,48 @@
+import todoStore from '../store/todo.store';
 import html from './app.html?raw';
+import { renderTodos } from './use-cases';
+
+const ElementIDs = {
+    TodoList: '.todo-list',
+    NewTodoInput: '#new-todo-input',
+}
+
 /**
  * 
  * @param {String} elementId 
  */
 
 export const App = ( elementId ) => {
+
+    const displayTodos = () => {
+            const todos = todoStore.getTodos( todoStore.getCurrentFilter() );
+            renderTodos(ElementIDs.TodoList, todos );
+    }
     // Funcion autoinvocada
     (() => {
         const app = document.createElement('div');
         app.innerHTML = html;
         document.querySelector( elementId ).append( app );
+        displayTodos();
 
     })();
+
+    // Referencias HTML
+    const newDescriptionInput = document.querySelector( ElementIDs.NewTodoInput );
+
+    // Listeners
+    newDescriptionInput.addEventListener('keyup', (event) => {
+        if ( event.keyCode !== 13 ) return;
+        if ( event.target.value.trim().length === 0 ) return;
+
+        todoStore.addTodo( event.target.value ); // añade el valor del input a Todo
+
+        displayTodos(); // Lista todos
+
+        event.target.value = ''; // Borra el input
+        
+        
+    });
 
 
 
