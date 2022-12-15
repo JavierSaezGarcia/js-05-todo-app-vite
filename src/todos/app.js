f3c00194b536c684d9d47321c90de94c3186703c
Javier Saez <banbleac@gmail.com>
2022-12-15 10:51:18 +0100

    html y css

---
+++ src/todos/app.js
@@ -0,0 +1,20 @@
+import html from './app.html?raw';
+/**
+ * 
+ * @param {String} elementId 
+ */
+
+export const App = ( elementId ) => {
+    // Funcion autoinvocada
+    (() => {
+        const app = document.createElement('div');
+        app.innerHTML = html;
+        document.querySelector( elementId ).append( app );
+
+    })();
+
+
+
+
+
+}
